e35844b3f4d7686e2432f2c5822cede09511884d
qkitzero <qkitzero@gmail.com>
2024-12-17 00:02:41 +0900

    add: application

---
+++ intarnal/application/user/service.go
@@ -0,0 +1,16 @@
+package user
+
+import "user/intarnal/domain/user"
+
+type UserService struct {
+	repo user.UserRepository
+}
+
+func NewUserService(repo user.UserRepository) *UserService {
+	return &UserService{repo: repo}
+}
+
+func (s *UserService) CreateUser(name, email string) error {
+	user := user.NewUser("id", name, email)
+	return s.repo.Save(user)
+}
