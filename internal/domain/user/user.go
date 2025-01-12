cd2421dac6b9db4e7dd47912fe3df172415dad1a
qkitzero <qkitzero@gmail.com>
2025-01-12 20:18:22 +0900

    fix: user

--- internal/domain/user/user.go
+++ internal/domain/user/user.go
@@ -5,14 +5,12 @@ import "time"
 type User interface {
 	ID() UserID
 	DisplayName() DisplayName
-	Email() Email
 	CreatedAt() time.Time
 }
 
 type user struct {
 	id          UserID
 	displayName DisplayName
-	email       Email
 	createdAt   time.Time
 }
 
@@ -24,20 +22,14 @@ func (u user) DisplayName() DisplayName {
 	return u.displayName
 }
 
-func (u user) Email() Email {
-	return u.email
-}
-
 func (u user) CreatedAt() time.Time {
 	return u.createdAt
 }
 
-func NewUser(id UserID, displayName DisplayName, email Email) User {
-	now := time.Now()
+func NewUser(id UserID, displayName DisplayName, createdAt time.Time) User {
 	return user{
 		id:          id,
 		displayName: displayName,
-		email:       email,
-		createdAt:   now,
+		createdAt:   createdAt,
 	}
 }
