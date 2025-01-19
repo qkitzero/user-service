6a8a73b9862095444eb6af2305f0fa7dc76c0ecd
qkitzero <qkitzero@gmail.com>
2025-01-19 15:23:34 +0900

    fix: user id

--- internal/domain/user/user_id.go
+++ internal/domain/user/user_id.go
@@ -1,11 +1,19 @@
 package user
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 type UserID struct {
 	uuid.UUID
 }
 
-func NewUserID() UserID {
-	return UserID{uuid.New()}
+func NewUserID(s string) (UserID, error) {
+	id, err := uuid.Parse(s)
+	if err != nil {
+		return UserID{}, fmt.Errorf("invalid UUID format: %w", err)
+	}
+	return UserID{id}, nil
 }
