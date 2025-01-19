617946b0ce3d6340cd48b7b5dc3a80206ef766e4
qkitzero <qkitzero@gmail.com>
2025-01-19 15:24:14 +0900

    fix: user handler

--- internal/interface/grpc/user/handler.go
+++ internal/interface/grpc/user/handler.go
@@ -2,23 +2,34 @@ package user
 
 import (
 	"context"
+	"user/internal/application/auth"
 	"user/internal/application/user"
 	"user/pb"
 )
 
 type UserHandler struct {
 	pb.UnimplementedUserServiceServer
+	authService auth.AuthService
 	userService user.UserService
 }
 
-func NewUserHandler(userService user.UserService) *UserHandler {
+func NewUserHandler(
+	authService auth.AuthService,
+	userService user.UserService,
+) *UserHandler {
 	return &UserHandler{
+		authService: authService,
 		userService: userService,
 	}
 }
 
 func (h *UserHandler) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
-	user, err := h.userService.CreateUser(req.GetDisplayName())
+	userID, err := h.authService.VerifyToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	user, err := h.userService.CreateUser(userID, req.GetDisplayName())
 	if err != nil {
 		return nil, err
 	}
