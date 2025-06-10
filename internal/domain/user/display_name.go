deadf3ffeb19a04a035e7ced7fd5674d6fad5110
qkitzero <qkitzero@gmail.com>
2025-06-10 20:49:25 +0900

    fix: display name

--- internal/domain/user/display_name.go
+++ internal/domain/user/display_name.go
@@ -14,7 +14,7 @@ func (d DisplayName) String() string {
 func NewDisplayName(s string) (DisplayName, error) {
 	s = strings.TrimSpace(s)
 	if s == "" {
-		return "", fmt.Errorf("invalid display name")
+		return DisplayName(""), fmt.Errorf("invalid display name")
 	}
 	return DisplayName(s), nil
 }
