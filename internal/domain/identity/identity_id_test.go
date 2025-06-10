513312655106ed29f10bae9df9dd35e8876f0c40
qkitzero <qkitzero@gmail.com>
2025-06-10 20:48:47 +0900

    add: test

---
+++ internal/domain/identity/identity_id_test.go
@@ -0,0 +1,26 @@
+package identity
+
+import "testing"
+
+func TestNewIdentityID(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		success bool
+		id      string
+	}{
+		{"success new identity id", true, "google-oauth2|000000000000000000000"},
+		{"failure empty identity id", false, ""},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewIdentityID(tt.id)
+			if tt.success && err != nil {
+				t.Errorf("expected no error, but got %v", err)
+			}
+			if !tt.success && err == nil {
+				t.Errorf("expected error but got nil")
+			}
+		})
+	}
+}
